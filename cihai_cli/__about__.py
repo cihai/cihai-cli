385f000ac9b9c67ae0ba29a510e06bec534a287e
Tony Narlock <tony@git-pull.com>
2017-05-20 02:06:39 -0500

    add initial module

---
+++ cihai_cli/__about__.py
@@ -0,0 +1,8 @@
+__title__ = 'cihai-cli'
+__package_name__ = 'cihai_cli'
+__version__ = '0.2.0'
+__description__ = 'Command line frontend for the cihai CJK language library'
+__author__ = 'Tony Narlock'
+__email__ = 'tony@git-pull.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Tony Narlock'
