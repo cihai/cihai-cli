ea22f4b81a4de42e285ae2450e3ee91b35ce1d88
Tony Narlock <tony@git-pull.com>
2020-08-09 12:30:11 -0500

    build(cihai-cli): Tag v0.6.0a1

--- cihai_cli/__about__.py
+++ cihai_cli/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'cihai-cli'
 __package_name__ = 'cihai_cli'
-__version__ = '0.5.1'
+__version__ = '0.6.0a1'
 __description__ = 'Command line frontend for the cihai CJK language library'
 __author__ = 'Tony Narlock'
 __email__ = 'tony@git-pull.com'
