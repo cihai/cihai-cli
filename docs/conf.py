ec72de4d97876a7a1999f86098518571a0eca3cd
Tony Narlock <tony@git-pull.com>
2021-06-16 09:30:45 -0500

    docs: Add mypy support

--- docs/conf.py
+++ docs/conf.py
@@ -28,13 +28,14 @@ extensions = [
     'alagitpull',
     'sphinx_click.ext',  # sphinx-click
     'sphinx_issues',
+    'myst_parser',
 ]
 
 issues_github_path = about['__github__'].replace('https://github.com/', '')
 
 templates_path = ['_templates']
 
-source_suffix = '.rst'
+source_suffix = {'.rst': 'restructuredtext', '.md': 'markdown'}
 
 master_doc = 'index'
 
