57aab965ddd5bf9b06d957dc5154c3ade02c6fec
Tony Narlock <tony@git-pull.com>
2022-03-06 15:15:47 -0600

    refactor(test): tmpdir -> tmp_path

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 import zipfile
 
 import pytest
@@ -17,8 +18,8 @@ def test_config_file(fixture_path):
 
 
 @pytest.fixture
-def zip_path(tmpdir):
-    return tmpdir.join("Unihan.zip")
+def zip_path(tmp_path):
+    return tmp_path / "Unihan.zip"
 
 
 @pytest.fixture
@@ -34,14 +35,14 @@ def zip_file(zip_path, fixture_path):
 
 
 @pytest.fixture
-def unihan_options(zip_file, zip_path, tmpdir):
+def unihan_options(zip_file, zip_path, tmp_path: pathlib.Path):
     return {
         "source": str(zip_path),
-        "work_dir": str(tmpdir),
-        "zip_path": str(tmpdir.join("downloads").join("Moo.zip")),
+        "work_dir": str(tmp_path),
+        "zip_path": str(tmp_path / "Downloads" / "Moo.zip"),
     }
 
 
 @pytest.fixture(scope="function")
-def tmpdb_file(tmpdir):
-    return tmpdir.join("test.db")
+def tmpdb_file(tmp_path: pathlib.Path):
+    return tmp_path / "test.db"
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,5 @@
+import pathlib
+
 import pytest
 
 import yaml
@@ -17,13 +19,17 @@ def test_cli(test_config_file):
     # assert result.exit_code == 0
 
 
-def test_cli_reflects_after_bootstrap(tmpdir, tmpdb_file, unihan_options):
+def test_cli_reflects_after_bootstrap(
+    tmp_path: pathlib.Path, tmpdb_file, unihan_options
+):
     config = {
         "database": {"url": f"sqlite:///{tmpdb_file}s"},
         "unihan_options": unihan_options,
     }
-    config_file = tmpdir.join("config.yml")
-    config_file.write(yaml.dump(config, default_flow_style=False))
+    config_file = tmp_path / "config.yml"
+    config_file.write_text(
+        yaml.dump(config, default_flow_style=False), encoding="utf-8"
+    )
     runner = CliRunner()
     result = runner.invoke(cli.cli, ["-c", str(config_file), "info", "㐀"])
     assert "Bootstrapping Unihan database" in result.output
