177918f0b6cccb88e03a5f8d8399868b8680def7
Tony Narlock <tony@git-pull.com>
2023-06-03 16:46:22 -0500

    refactor!: Move to pathlib + tmp_path
    See also:
    - https://docs.python.org/3/library/pathlib.html
    - https://docs.pytest.org/en/7.1.x/how-to/tmp_path.html#the-tmp-path-fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+import typing as t
 import zipfile
 
 import pytest
@@ -7,42 +8,53 @@ import pytest
 from cihai.data.unihan.constants import UNIHAN_FILES
 
 
-@pytest.fixture
-def fixture_path():
-    return os.path.abspath(os.path.join(os.path.dirname(__file__), "fixtures"))
+if t.TYPE_CHECKING:
+    from cihai.types import UntypedDict as UnihanOptions
 
 
 @pytest.fixture
-def test_config_file(fixture_path):
+def tests_path() -> pathlib.Path:
+    return pathlib.Path(__file__).parent
+
+
+@pytest.fixture
+def fixture_path(tests_path: pathlib.Path) -> pathlib.Path:
+    return tests_path / "fixtures"
+
+
+@pytest.fixture
+def test_config_file(fixture_path: str) -> str:
     return os.path.join(fixture_path, "test_config.yml")
 
 
 @pytest.fixture
-def zip_path(tmp_path):
+def zip_path(tmp_path: pathlib.Path) -> pathlib.Path:
     return tmp_path / "Unihan.zip"
 
 
 @pytest.fixture
-def zip_file(zip_path, fixture_path):
+def zip_file(zip_path: pathlib.Path, fixture_path: pathlib.Path) -> zipfile.ZipFile:
     _files = []
     for f in UNIHAN_FILES:
-        _files += [os.path.join(fixture_path, f)]
-    zf = zipfile.ZipFile(str(zip_path), "a")
-    for f in _files:
-        zf.write(f, os.path.basename(f))
+        _files += [fixture_path / f]
+    zf = zipfile.ZipFile(zip_path, "a")
+    for _f in _files:
+        zf.write(_f, _f.name)
     zf.close()
     return zf
 
 
 @pytest.fixture
-def unihan_options(zip_file, zip_path, tmp_path: pathlib.Path):
+def unihan_options(
+    zip_file: zipfile.ZipFile, zip_path: pathlib.Path, tmp_path: pathlib.Path
+) -> "UnihanOptions":
     return {
-        "source": str(zip_path),
-        "work_dir": str(tmp_path),
-        "zip_path": str(tmp_path / "Downloads" / "Moo.zip"),
+        "source": zip_path,
+        "work_dir": tmp_path,
+        "zip_path": tmp_path / "downloads" / "Moo.zip",
     }
 
 
 @pytest.fixture(scope="function")
-def tmpdb_file(tmp_path: pathlib.Path):
-    return tmp_path / "test.db"
+def tmpdb_file(tmpdir: pathlib.Path) -> pathlib.Path:
+    return tmpdir / "test.db"
