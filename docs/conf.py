53274a84d2bb97e2da75685c3eb2108fb0f230c3
Tony Narlock <tony@git-pull.com>
2023-09-02 07:40:14 -0500

    docs(typos): typos --format brief --write-changes

--- docs/conf.py
+++ docs/conf.py
@@ -121,7 +121,7 @@ simplify_optional_unions = True
 # sphinxext.opengraph
 ogp_site_url = about["__docs__"]
 ogp_image = "_static/img/icons/icon-192x192.png"
-ogp_desscription_length = about["__description__"]
+ogp_description_length = about["__description__"]
 ogp_site_name = about["__title__"]
 
 # sphinx-copybutton
